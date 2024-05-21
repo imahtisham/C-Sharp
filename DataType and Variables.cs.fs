545b0a1c929b344c9f12c3476d87577ed64a83f7
Muhammad Ehtisham Zafar <ahtizafi@gmail.com>
2024-05-21 19:45:15 +0500

    Add files via upload

---
+++ DataType and Variables.cs.fs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Security;
+namespace MyTask
+{
+    class Education
+    {
+        static void Main (string[] args)
+        {
+            int MyNumber = 200;
+            char Myletter ='A';
+            double DecimalValue = 19.99;
+            bool Pass = true;
+            bool Fail = false;
+            string text =  "Hello";
+           Console.WriteLine(MyNumber);
+           Console.WriteLine(Myletter);
+           Console.WriteLine(DecimalValue);
+           Console.WriteLine(Fail);
+           Console.WriteLine(text);
+           Console.WriteLine(Pass);
+
+        }
+
+    }
+}
