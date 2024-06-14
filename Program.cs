0ea19c1b5af42e6249764c6c9261e67c990cb37d
Muhammad Ehtisham Zafar <ahtizafi@gmail.com>
2024-06-14 19:27:47 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,62 +1,23 @@
 ﻿using System;
-using System.Net.Security;
-namespace myend
+namespace Practice
 {
-    class Program
+    class MyApplication
     {
         static void Main(string[]args)
         {
-            Console.WriteLine( "Enter Code");
-            string code = Console.ReadLine();
-            if(code== "heading")
+            int i;
+            Console.WriteLine("Enter a Number to Proceed");
+            Console.ReadLine();
+            if(i==18)
             {
-                Console.WriteLine("<h1></h1>");
+                Console.WriteLine("ATGN  You are eligible for this post" );
 
-            };
-
-            Console.WriteLine("Enter code2");
-            string code2= Console.ReadLine();
-            if(code2=="navbar")
-            {
-                Console.WriteLine("  {<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>}   ");
             }
+            else{
 
-         
+                Console.WriteLine("Sorry!!");
+            }
         }
+
     }
 }
