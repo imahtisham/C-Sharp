99a542831450fddd04d8c551d63beedcbcd25c44
Muhammad Ehtisham Zafar <ahtizafi@gmail.com>
2024-06-14 19:25:25 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
-namespace MyApplication
+namespace conditions
 {
     public class Program
     {
-static void Main()
-{
-    int valueOne;
-    int valueTwo;
-    Console.WriteLine("Please Enter Number valueOne");
-    valueOne = Convert.ToInt32 (Console.ReadLine());
-    Console.WriteLine("Please Enter Number valurTwo");
-    valueTwo = Convert.ToInt32(Console.ReadLine());
-    int Result = valueOne+valueTwo;
-    //Console.WriteLine(Result); 
-    Console.WriteLine("Result is two value are add" +Result);
-}
+        static void Main()
+        {
+            int k=20;
+        
+            Console.WriteLine("Enter Your Age");
+            Console.ReadLine();
+           if(k<20)
+          {
+            Console.WriteLine("You are eligible to caste a vote");
+            }
+            else
+            {
+                Console.WriteLine("Sorry Age Issue");
+            }
+          }
+            
+        }
     }
-    
-}
