2c277f7f328365adaca09f5a0a32152ef8141864
Muhammad Ehtisham Zafar <ahtizafi@gmail.com>
2024-06-14 19:24:50 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MyApplication
+{
+    public class Program
+    {
+static void Main()
+{
+    int valueOne;
+    int valueTwo;
+    Console.WriteLine("Please Enter Number valueOne");
+    valueOne = Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine("Please Enter Number valurTwo");
+    valueTwo = Convert.ToInt32(Console.ReadLine());
+    int Result = valueOne+valueTwo;
+    //Console.WriteLine(Result); 
+    Console.WriteLine("Result is two value are add" +Result);
+}
+    }
+    
+}
